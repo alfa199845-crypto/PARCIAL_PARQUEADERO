b263295c9433bf88ceacf14313620b71eda67b2a
alfa199845-crypto <alfa199845@gmail.com>
2026-04-19 10:33:45 -0500

    modifique el main y utilice for para inicializar matriz del parqueadero, utilizo

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
 void mostrarmapa();
 
 int main() {
+
+    // Inicializamos todo el parqueadero vacío
+    for(int i = 0; i < 16; i++) {
+        for(int j = 0; j < 16; j++) {
+            parqueadero[i][j].ocupado = false;
+            parqueadero[i][j].placa = "";
+        }
+    }
+
+    // Mostrar el mapa
     mostrarmapa();
+
     return 0;
 }
