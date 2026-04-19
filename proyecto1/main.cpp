6597ea1ffdf29b1177ed5fb0bb26697d81d95b91
ternemi13 <wilsperez13@gmail.com>
2026-04-19 11:03:18 -0500

    uso la funcion ingresarvehiculo y la declaro

--- proyecto1/main.cpp
+++ proyecto1/main.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 void mostrarmapa();
 void inicializarParqueadero();
+void ingresarvehiculo();
 
 int main() {
 
     inicializarParqueadero();
+
+    ingresarvehiculo();
     // Mostrar el mapa
     mostrarmapa();
+
+    
 
     return 0;
 }
